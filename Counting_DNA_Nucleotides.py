6d7e128cf0614b2cb992b345007f979b8a674589
mdrzal <74113447+mdrzal@users.noreply.github.com>
2022-03-10 19:37:08 +0000

    Update Counting_DNA_Nucleotides.py

--- Counting_DNA_Nucleotides.py
+++ Counting_DNA_Nucleotides.py
@@ -1,4 +1,5 @@
 from collections import Counter
+#insert the strin as s
 s="AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
 s=Counter(list(s))
 print(s["A"],s["C"],s["G"],s["T"])
