4a729e673404913c1090de76d4fa361b2d60e8c9
mdrzal <74113447+mdrzal@users.noreply.github.com>
2022-03-21 22:21:51 +0000

    Create Finding_a_Motif_in_DNA.py

---
+++ Finding_a_Motif_in_DNA.py
@@ -0,0 +1,15 @@
+in_string="""GATATATGCATATACTT
+ATAT"""
+
+A=in_string.split('\n')
+
+s=A[0]
+s1=A[1]
+#%%
+A1=list()
+for i in range(len(s)):
+    if s[i:].startswith(f"{s1}"):
+        keep=str(i+1)
+        A1.append(keep)
+        
+print(" ".join(A1))
